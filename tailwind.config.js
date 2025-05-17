09b865925cb8cee6bce36c237c97ba8cae406d46
Safmica <safmikasa@gmail.com>
2025-05-18 02:32:19 +0700

    feat: add homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                "cos-blue": "#175cd3",
+                "cos-red": "#d62828",
+                "cos-yellow": "#fcbf49",
+                "cos-light-yellow": "#fad991",
+                "cos-dark-yellow": "#dea12a",
+                "cos-orange": "#f77f00",
+                "cos-cream": "#eae2b7",
             },
         },
     },
